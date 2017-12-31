fc72a5d57494c0c2402118aad8ed1beea7ddfbe9
androlopez1 <androlopez1@gmail.com>
2017-12-31 15:34:01 -0500

    'jwt-hapi'

---
+++ README.txt
@@ -0,0 +1,9 @@
+social-media-oauth-test
+
+port: 3000
+
+For generate token open: localhost:3000/jwt
+Note: Token is generated using SECRET and KEY given by consumer "user" created in kong server.
+
+For social-media authentication open: localhost:3000/login
+Note: Login test for TWITTER.
--- index.js
+++ index.js
@@ -3,34 +3,25 @@
 const Hapi = require('hapi');
 const jwt = require('jwt-simple');
 
-var KEY = "QzXl5fGWo5mzQednABoFIzriTeZhBgYm";
-var SECRET = "oqYe3GX1xPu5gziymPYHLuf6VfJNCaON";
+var KEY = //"TYPE YOUR KEY HERE";
+var SECRET = //"TYPE YOUR SECRET HERE";
 const server = new Hapi.Server();
 
 server.connection({ port: 3000, host: 'localhost' });
 
-// Register bell with the server
 server.register(require('bell'), function (err) {
- 
-    // Declare an authentication strategy using the bell scheme
-    // with the name of the provider, cookie encryption password,
-    // and the OAuth client credentials.
+
     server.auth.strategy('twitter', 'bell', {
         provider: 'twitter',
         password: 'cookie_encryption_password_secure',
-        clientId: 'fRV9eeGT29UFqV11eFo1ZxqPP',
-        clientSecret: 'uU1M1ucHEYHck0ygtnUBOmB4ZrLNa56RgCoFqPxsU3uIWsTzsZ', 
-        isSecure: false     // Terrible idea but required if not using HTTPS especially if developing locally
+        clientId: //'',
+        clientSecret:// '', 
+        isSecure: false     
     });
 
-    // Use the 'twitter' authentication strategy to protect the
-    // endpoint handling the incoming authentication credentials.
-    // This endpoints usually looks up the third party account in
-    // the database and sets some application state (cookie) with
-    // the local application account information.
     server.route({
-        method: ['GET', 'POST'], // Must handle both GET and POST
-        path: '/login',          // The callback endpoint registered with the provider
+        method: ['GET', 'POST'], 
+        path: '/login',          
         config: {
             auth: 'twitter',
             handler: function (request, reply) {
@@ -38,11 +29,7 @@ server.register(require('bell'), function (err) {
                 if (!request.auth.isAuthenticated) {
                     return reply('Authentication failed due to: ' + request.auth.error.message);
                 }
- 
-                // Perform any account lookup or registration, setup local session,
-                // and redirect to the application. The third-party credentials are
-                // stored in request.auth.credentials. Any query parameters from
-                // the initial request are passed back via request.auth.credentials.query.
+
                 return reply('Hello!');
             }
         }
@@ -61,7 +48,6 @@ server.route({
 	}
 });
 
-  // Start the server
   server.start((err) => {
 
     if (err) {
